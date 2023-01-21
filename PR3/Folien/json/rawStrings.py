01978de299960457df6afb29c083c57fc2093eec
Ni-o-iN <nico_ohler@hotmail.de>
2023-01-21 22:36:23 +0100

    alles rüber

---
+++ PR3/Folien/json/rawStrings.py
@@ -0,0 +1,10 @@
+print("Eine Zeile")
+print("Zwei\nZeilen")
+
+print(r"Hello" == "Hello")
+print(r"\the" == "\\the")
+print(r"\the" == "\the")
+print(r"\the")
+print("\the")
+
+print(r"C:\Users\Nico_\Downloads")
